f3e43910c180a06f1f267c28a8fd9aac0cf8e17b
Leosousa-dev <leonardocasa790@gmail.com>
2025-04-02 19:39:33 -0300

    re-initializing project

--- src/index.ts
+++ src/index.ts
@@ -6,27 +6,7 @@ import * as cheerio from "cheerio"
 
 async function crawler(url: string) {
     try {
-        console.log(`initialize crowler in :${url}`)
-
-        // 1 make request to url
-        const response = await axios.get(url);
-
-        // 2 loading html
-        const $ = cheerio.load(response.data)
-
-        // 3 extrate data 
-        const paragraph: string[] = []
-
-        $('p').each((index, element) => {
-            const href = $(element).attr('herf')
-            if(href){
-                paragraph.push(href)
-            }
-        })
-
-        // 4 show to result
-        console.log(`Encontrados: ${paragraph.length} paragraph na pagina:`)
-        console.log(paragraph.slice(0,10));
+       
     }
     catch(err){
         console.log("Erro durante a execucao", err)
